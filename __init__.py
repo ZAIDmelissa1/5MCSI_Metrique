d6db3f416f77016ab74a74d471f9e370e6c9597a
ZAIDmelissa1 <159758864+ZAIDmelissa1@users.noreply.github.com>
2024-02-12 11:41:54 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -29,6 +29,10 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
+
 
 if __name__ == "__main__":
   app.run(debug=True)
